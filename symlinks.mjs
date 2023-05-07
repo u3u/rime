49625dfc7a5a4ee2f84a82e54ee5d0686809067d
qwq <qwq@qwq.cat>
2023-05-07 17:36:51 +0800

    refactor: improve `symlinks` script

--- symlinks.mjs
+++ symlinks.mjs
@@ -1,19 +1,31 @@
-import fs from 'fs/promises'
+// @ts-check
+import fs from 'node:fs/promises'
+import path from 'node:path'
 import chalk from 'chalk'
 import { globby } from 'globby'
 
-const name = 'rime-ice'
-const excludes = ['.gitignore', 'README.md', 'LICENSE', 'custom_phrase.txt']
+const cwd = 'rime-ice'
+const ignore = ['.gitignore', 'README.md', 'LICENSE', 'custom_phrase.txt']
 
-const paths = await globby([`${name}/*`, ...excludes.map((item) => `!${name}/${item}`)], {
+const paths = await globby('*', {
+  cwd,
+  ignore,
   onlyFiles: false,
 })
 
 await Promise.allSettled(
-  paths.map(async (path) => {
-    await fs.symlink(path, path.replace(`${name}/`, ''))
-    console.log(`${chalk.green('✔')} ${path}`)
+  paths.map(async (item) => {
+    const file = path.join(cwd, item)
+    const link = `${chalk.magenta(item)} -> ${file}`
+
+    try {
+      await fs.rm(item)
+      await fs.symlink(file, item)
+      console.log(chalk.green('✔'), link)
+    } catch {
+      console.log(chalk.gray(`skip ${link}`))
+    }
   })
 )
 
-console.log(`${chalk.green('✔')} Create symlinks done.`)
+console.log(chalk.green('✔'), 'Create symlinks done.')
